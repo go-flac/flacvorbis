e2254dd77f6b5ca568a69a78e54b84d52994e612
eternal-flame-AD <yume@yumechi.jp>
2024-08-02 00:10:28 -0500

    Migrate to go-flac v2 (go-flac/go-flac#5)
    Signed-off-by: eternal-flame-AD <yume@yumechi.jp>

---
+++ v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/go-flac/flacvorbis
+
+go 1.20
+
+require github.com/go-flac/go-flac/v2 v2.0.1
---
+++ v2/vorbis_test.go
@@ -0,0 +1,126 @@
+package flacvorbis
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	flac "github.com/go-flac/go-flac/v2"
+)
+
+func httpGetBytes(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP status %d", res.StatusCode)
+	}
+	return ioutil.ReadAll(res.Body)
+}
+
+func TestNewVorbisComment(t *testing.T) {
+	cmt := New()
+	if !strings.HasPrefix(cmt.Vendor, "flacvorbis") {
+		t.Errorf("Unexpected vendor: %s\n", cmt.Vendor)
+		t.Fail()
+	}
+	if len(cmt.Comments) != 0 {
+		t.Error("Unexpected comments: ", cmt.Comments)
+		t.Fail()
+	}
+}
+func TestVorbisFromExistingFlac(t *testing.T) {
+	zipdata, err := httpGetBytes("http://helpguide.sony.net/high-res/sample1/v1/data/Sample_BeeMoved_96kHz24bit.flac.zip")
+	if err != nil {
+		t.Errorf("Error while downloading test file: %s", err.Error())
+		t.FailNow()
+	}
+	zipfile, err := zip.NewReader(bytes.NewReader(zipdata), int64(len(zipdata)))
+	if err != nil {
+		t.Errorf("Error while decompressing test file: %s", err.Error())
+		t.FailNow()
+	}
+	if zipfile.File[0].Name != "Sample_BeeMoved_96kHz24bit.flac" {
+		t.Errorf("Unexpected test file content: %s", zipfile.File[0].Name)
+		t.FailNow()
+	}
+
+	flachandle, err := zipfile.File[0].Open()
+	if err != nil {
+		t.Errorf("Failed to decompress test file: %s", err)
+		t.FailNow()
+	}
+
+	f, err := flac.ParseBytes(flachandle)
+	if err != nil {
+		t.Errorf("Failed to parse flac file: %s", err)
+		t.FailNow()
+	}
+
+	var cmt *MetaDataBlockVorbisComment
+	for _, meta := range f.Meta {
+		if meta.Type == flac.VorbisComment {
+			cmt, err = ParseFromMetaDataBlock(*meta)
+			if err != nil {
+				t.Errorf("Error while parsing metadata image: %s\n", err.Error())
+				t.Fail()
+			}
+		}
+	}
+
+	if err := cmt.Add(FIELD_GENRE, "Bee Pop"); err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	check := func(cmt *MetaDataBlockVorbisComment) {
+		if cmt.Vendor != "reference libFLAC 1.2.1 win64 20080709" {
+			t.Errorf("Unexpected vendor string: %s\n", cmt.Vendor)
+			t.Fail()
+		}
+		if res, err := cmt.Get(FIELD_ALBUM); err != nil {
+			t.Error(err)
+			t.Fail()
+		} else if len(res) != 1 || res[0] != "Bee Moved" {
+			t.Error("Unexpected album name: ", res)
+			t.Fail()
+		}
+
+		if res, err := cmt.Get(FIELD_ARTIST); err != nil {
+			t.Error(err)
+			t.Fail()
+		} else if len(res) != 1 || res[0] != "Blue Monday FM" {
+			t.Error("Unexpected artist name: ", res)
+			t.Fail()
+		}
+
+		if res, err := cmt.Get(FIELD_TITLE); err != nil {
+			t.Error(err)
+			t.Fail()
+		} else if len(res) != 1 || res[0] != "Bee Moved" {
+			t.Error("Unexpected title name: ", res)
+			t.Fail()
+		}
+
+		if res, err := cmt.Get(FIELD_GENRE); err != nil {
+			t.Error(err)
+			t.Fail()
+		} else if len(res) != 1 || res[0] != "Bee Pop" {
+			t.Error("Unexpected title name: ", res)
+			t.Fail()
+		}
+	}
+	check(cmt)
+	new, err := ParseFromMetaDataBlock(cmt.Marshal())
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	check(new)
+
+}
